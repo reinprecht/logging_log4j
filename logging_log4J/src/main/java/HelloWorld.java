ad3992156851420d3bdb9e45eca59073618ac16d
Stefan Reinprecht <stefan.reinprecht@gmail.com>
2013-09-12 10:32:33 +0200

    Commit everything for the first time

---
+++ logging_log4J/src/main/java/HelloWorld.java
@@ -0,0 +1,33 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+
+	private final static Logger logger = Logger.getLogger("TestLogger");
+
+	public void doABasicTest() {
+		logger.debug("This is a debug message");
+		logger.info("This is a info message");
+		logger.warn("This is a warn message");
+		logger.error("This is an error message");
+		logger.fatal("This is a fatal message");
+	}
+
+	public void doAnAdvancedTest() {
+		logger.info("Dann mal los.");
+
+		try {
+			((Object) null).toString();
+		} catch (Exception e) {
+			logger.error("oh oh", e);
+		}
+	}
+
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		BasicConfigurator.configure();
+		//hw.doABasicTest();
+		hw.doAnAdvancedTest();
+	}
+
+}
